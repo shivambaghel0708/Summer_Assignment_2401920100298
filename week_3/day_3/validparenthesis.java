649ec39bc7cf37661d91433ffb1ad3bee7b56e57
Shivam Baghel <shivambaghel@Shivams-MacBook-Air-2.local>
2026-06-17 23:55:48 +0530

    week 3 day 3 satck

---
+++ week_3/day_3/validparenthesis.java
@@ -0,0 +1,30 @@
+package week_3.day_3;
+
+import java.util.Stack;
+
+public class validparenthesis {
+    class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char top = stack.pop();
+                if ((ch == ')' && top != '(') ||
+                    (ch == '}' && top != '{') ||
+                    (ch == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
+    
+}
